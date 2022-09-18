24ff0e007cca7177cf9d904de0a4cad2f7fe98b0
Elvyn Mejia <elvynmejia@gmail.com>
2022-09-17 18:08:59 -0700

    adds api endpoint tests

--- spec/app.test.js
+++ spec/app.test.js
@@ -8,4 +8,82 @@ describe('currency conversion service', () => {
             expect(response.text).to.eq('App running Ok!');
         });
     });
+
+    describe('/api/v1/conversions', () => {
+        context('success', () => {
+            it('convert currency', async () => {
+                const response = await global.request
+                    .post('/api/v1/conversions')
+                    .send({
+                        from: 'BTC',
+                        to: 'USD',
+                        amount: '999.20',
+                    });
+                expect(response.status).to.equal(200);
+
+                expect(
+                    Object.keys(response.body),
+                ).to.have.members([
+                    'from',
+                    'to',
+                    'amount',
+                    'exchangeRate',
+                    'newAmout',
+                    'toRate',
+                    'fromRate',
+                ]);
+
+                Object.values(response.body).forEach((item) => {
+                    expect(item).to.not.eq(null);
+                });
+            });
+        });
+
+        context('errors', () => {
+            it('missing from in the request body', async () => {
+                const response = await global.request
+                    .post('/api/v1/conversions')
+                    .send({
+                        // from: 'BTC',
+                        to: 'USD',
+                        amount: '999.20',
+                    });
+                expect(response.status).to.equal(422);
+                expect(response.body).to.deep.eq({
+                    errors: [{ message: '"from" is required' }],
+                    message: 'Unprocessable Entity',
+                });
+            });
+
+            it('missing to in the request body', async () => {
+                const response = await global.request
+                    .post('/api/v1/conversions')
+                    .send({
+                        from: 'BTC',
+                        // to: 'USD',
+                        amount: '999.20',
+                    });
+                expect(response.status).to.equal(422);
+                expect(response.body).to.deep.eq({
+                    errors: [{ message: '"to" is required' }],
+                    message: 'Unprocessable Entity',
+                });
+            });
+
+            it('missing amount in the request body', async () => {
+                const response = await global.request
+                    .post('/api/v1/conversions')
+                    .send({
+                        from: 'BTC',
+                        to: 'USD',
+                        // amount: '999.20',
+                    });
+                expect(response.status).to.equal(422);
+                expect(response.body).to.deep.eq({
+                    errors: [{ message: '"amount" is required' }],
+                    message: 'Unprocessable Entity',
+                });
+            });
+        });
+    });
 });
