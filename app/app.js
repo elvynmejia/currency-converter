3b40dcb5abd7941857b4e58ec023faf64fcc345a
Elvyn Mejia <elvynmejia@gmail.com>
2022-09-17 19:37:51 -0700

    adds api rate limiter

--- app/app.js
+++ app/app.js
@@ -1,9 +1,23 @@
+const rateLimit = require('express-rate-limit');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
+const { DateTime } = require('luxon-business-days');
+
+const now = DateTime.now()
+
+const rateLimiter = rateLimit({
+    windowMs: 24 * 60 * 60 * 1000, // 24 hrs in milliseconds
+    max: now.isBusinessDay() ? 100 : 200, // 100 per workday 200 on weekends
+    legacyHeaders: false,
+    standardHeaders: true,
+});
+
 const { conversionsRouter } = require('./api/v1');
+
 const {
     unprocessableEntityErrorHandler,
     unauthorizedErrorHandler,
@@ -17,6 +31,7 @@ const logger = console;
 const app = express();
 const port = 3001;
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(morgan('combined'));
 app.use(bodyParser.urlencoded({ extended: false }));
